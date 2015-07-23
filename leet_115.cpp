d11d3e763d6598da7a1a5b1be166dbe0c6e49fcc
xiaoxiamii <cj766@qq.com>
2015-07-23 11:30:55 +0800

    115

---
+++ leet_115.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include<string>
+#include<stack>
+#include<algorithm>
+#include<queue>
+#include<limits>
+#include<numeric>
+#include<sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int c() {
+        cout << "test" << endl;
+        return 0;
+    }
+
+};
+
+int main() {
+    Solution s;
+    s.c();
+
+    return 0;
+}
